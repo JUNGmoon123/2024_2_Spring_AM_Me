74cfd0619dbbf06e5806e90f7611733bd7c18f5e
JUNGmoon123 <jungpro567@gmail.com>
2024-02-15 16:43:08 +0900

    좋아요, 싫어요 버튼 ajax로 처리

--- src/main/java/com/example/demo/controller/UsrReactionPointController.java
+++ src/main/java/com/example/demo/controller/UsrReactionPointController.java
@@ -10,8 +10,6 @@ import com.example.demo.util.Ut;
 import com.example.demo.vo.ResultData;
 import com.example.demo.vo.Rq;
 
-import jakarta.servlet.http.HttpServletRequest;
-
 @Controller
 public class UsrReactionPointController {
 
@@ -21,14 +19,9 @@ public class UsrReactionPointController {
 	@Autowired
 	private ReactionPointService reactionPointService;
 
-	// 액션 메서드
-
 	@RequestMapping("/usr/reactionPoint/doGoodReaction")
 	@ResponseBody
-//	public ResultData doGoodReaction(String relTypeCode, int relId, String replaceUri)
-	//기존 ResultData이던걸 Object로 바꿈, 이유로는 추천을 실행하고나서 detail쪽으로 다시 돌아가기 위해서 url을 넘겨받는걸 실행하려고.
-	//jsReplace로 들어가면 밑에 return Script언어로 location.replace명령어 사용됨. 싫어요도 마찬가지.
-	public Object doGoodReaction(String relTypeCode, int relId, String replaceUri){
+	public ResultData doGoodReaction(String relTypeCode, int relId, String replaceUri) {
 
 		ResultData usersReactionRd = reactionPointService.usersReaction(rq.getLoginedMemberId(), relTypeCode, relId);
 
@@ -36,13 +29,11 @@ public class UsrReactionPointController {
 
 		if (usersReaction == 1) {
 			ResultData rd = reactionPointService.deleteGoodReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
-			return Ut.jsReplace("S-1", "좋아요 취소", replaceUri);
-//					ResultData.from("S-1", "좋아요 취소");
+			return ResultData.from("S-1", "좋아요 취소");
 		} else if (usersReaction == -1) {
 			ResultData rd = reactionPointService.deleteBadReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
 			rd = reactionPointService.addGoodReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
-			return Ut.jsReplace("S-2", "싫어요 눌렀잖어", replaceUri);
-//					ResultData.from("S-2", "싫어요 눌렀잖어");
+			return ResultData.from("S-2", "싫어요 눌렀잖어");
 		}
 
 		ResultData reactionRd = reactionPointService.addGoodReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
@@ -51,14 +42,12 @@ public class UsrReactionPointController {
 			return ResultData.from(reactionRd.getResultCode(), reactionRd.getMsg());
 		}
 
-		return  Ut.jsReplace(reactionRd.getResultCode(), reactionRd.getMsg(), replaceUri);
-//				ResultData.from(reactionRd.getResultCode(), reactionRd.getMsg());
+		return ResultData.from(reactionRd.getResultCode(), reactionRd.getMsg());
 	}
 
 	@RequestMapping("/usr/reactionPoint/doBadReaction")
 	@ResponseBody
-//	public ResultData doBadReaction(String relTypeCode, int relId, String replaceUri)
-	public Object doBadReaction(String relTypeCode, int relId, String replaceUri){
+	public ResultData doBadReaction(String relTypeCode, int relId, String replaceUri) {
 
 		ResultData usersReactionRd = reactionPointService.usersReaction(rq.getLoginedMemberId(), relTypeCode, relId);
 
@@ -66,13 +55,11 @@ public class UsrReactionPointController {
 
 		if (usersReaction == -1) {
 			ResultData rd = reactionPointService.deleteBadReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
-			return Ut.jsReplace("S-1", "싫어요 취소", replaceUri);
-//			ResultData.from("S-1", "싫어요 취소");
+			return ResultData.from("S-1", "싫어요 취소");
 		} else if (usersReaction == 1) {
 			ResultData rd = reactionPointService.deleteGoodReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
 			rd = reactionPointService.addBadReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
-			return Ut.jsReplace("S-2", "좋아요 눌렀잖어", replaceUri);
-//			ResultData.from("S-2", "좋아요 눌렀잖어");
+			return ResultData.from("S-2", "좋아요 눌렀잖어");
 		}
 
 		ResultData reactionRd = reactionPointService.addBadReactionPoint(rq.getLoginedMemberId(), relTypeCode, relId);
@@ -81,8 +68,7 @@ public class UsrReactionPointController {
 			return ResultData.from(reactionRd.getResultCode(), reactionRd.getMsg());
 		}
 
-		return Ut.jsReplace(reactionRd.getResultCode(), reactionRd.getMsg(), replaceUri);
-//				ResultData.from(reactionRd.getResultCode(), reactionRd.getMsg());
+		return ResultData.from(reactionRd.getResultCode(), reactionRd.getMsg());
 	}
 
 }
